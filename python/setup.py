4eb9da15a309eb420cec5efb193cb8e0b3a28672
hanappe <peter@hanappe.com>
2023-07-23 10:23:56 +0200

    python interface

--- python/setup.py
+++ python/setup.py
@@ -1,17 +1,21 @@
-from distutils.core import setup, Extension
+from setuptools import setup
 from setuptools import find_packages
 import os
 
-setup (name = 'rcom',
-       description = 'The Python classes for the librcom communication library',
-       version = '0.1',
-       license='GPLv3',
-       author='Peter Hanappe',
-       download_url='https://github.com/romi/librcom',
-       # https://pypi.org/classifiers/
-       packages=find_packages(include=['rcom', 'rcom.*']),
-       classifiers=["Development Status :: 3 - Alpha",
-                    "Intended Audience :: Developers",
-                    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-                    "Programming Language :: Python :: 3",
-                    "Framework :: Robot Framework :: Library"])
+setup (
+    name = 'rcom',
+    description = 'The Python classes for the librcom communication library',
+    version = '0.1.0',
+    license = 'GPLv3',
+    author = 'Peter Hanappe',
+    download_url = 'https://github.com/romi/librcom',
+    packages = find_packages(include=['rcom', 'rcom.*']),
+    install_requires = [
+        'websocket-client'
+    ],
+    classifiers = ["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+                   "Programming Language :: Python :: 3",
+                   "Framework :: Robot Framework :: Library"]
+)
